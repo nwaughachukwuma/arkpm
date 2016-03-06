76ca5448df88d6cda75ba4a9d9f7f1e632dd628c
Matt.Nelson <git-bb@silverark.co.uk>
2016-03-06 22:06:31 +0000

    Add Timelog tranformer and updates routes

---
+++ app/Api/Transformers/TimelogTransformer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Api\Transformers;
+
+use App\Client;
+use League\Fractal\TransformerAbstract;
+
+class TimelogTransformer extends TransformerAbstract
+{
+	public function transform(Timelog $timelog)
+	{
+		return [
+            'id' => (int) $timelog->id,
+            'user_id' => (int) $timelog->user_id, 
+            'startdate' => $timelog->startdate, 
+            'enddate' => $timelog->enddate,
+            'minutes' => (int) $timelog->minutes, 
+            'client_id' => (int) $timelog->minutes, 
+            'project_id' => (int) $timelog->project_id, 
+            'task_id' => (int) $timelog->task_id, 
+            'billable' => (boolean) $timelog->billable, 
+            'visible' => (boolean) $timelog->visible
+		];
+	}
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,20 +23,20 @@ $api->version('v1', function ($api) {
 
 			$api->get('users/me', 'AuthController@me');
 			$api->get('validate_token', 'AuthController@validateToken');
-			
-			$api->get('dogs', 'DogsController@index');
-			$api->post('dogs', 'DogsController@store');
-			$api->get('dogs/{id}', 'DogsController@show');
-			$api->delete('dogs/{id}', 'DogsController@destroy');
-			$api->put('dogs/{id}', 'DogsController@update');
 
+			//Client endpoints
 			$api->get('clients', 'ClientsController@index');
 			$api->post('clients', 'ClientsController@store');
 			$api->get('clients/{id}', 'ClientsController@show');
 			$api->delete('clients/{id}', 'ClientsController@destroy');
 			$api->put('clients/{id}', 'ClientsController@update');
 
-
+			//Time Logging Functions
+			$api->get('timelogs', 'TrackingController@index');
+			$api->post('timelogs', 'TrackingController@store');
+			$api->get('timelogs/{id}', 'TrackingController@show');
+			$api->delete('timelogs/{id}', 'TrackingController@destroy');
+			$api->put('timelogs/{id}', 'TrackingController@update');
 
 		});
 
